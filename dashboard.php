66e1d6a1c9a2c87b5a95cf154c62d63005681a58
VARDHAN_VALLURI <krishnavardhan124@gmail.com>
2025-09-06 23:53:32 +0530

    dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -172,6 +172,12 @@ if (!isset($_SESSION['student'])) {
       </tr>
     </thead>
     <tbody>
+      <tr><td>05-sep-25</td><td>Fri</td><td>P</td><td>P</td><td>LIB</td><td>P</td></tr
+      <tr><td>04-sep-25</td><td>Thu</td><td>P</td><td>P</td><td>P</td><td>P</td></tr
+      <tr><td>03-sep-25</td><td>Wed</td><td>P</td><td>P</td><td>P</td><td>P</td></tr
+      <tr><td>02-sep-25</td><td>Tue</td><td>P</td><td>P</td><td>P</td><td>P</td></tr
+      <tr><td>01-Sep-25</td><td>Mon</td><td>P</td><td>P</td><td>P</td><td>P</td></tr
+      <tr><td>29-Aug-25</td><td>Fri</td><td>P</td><td>P</td><td>LIB</td><td>P</td></tr
       <tr><td>28-Aug-25</td><td>Thu</td><td>P</td><td>P</td><td>P</td><td>P</td></tr
       <tr><td>26-Aug-25</td><td>Tue</td><td>P</td><td>P</td><td>P</td><td>P</td></tr
       <tr><td>25-Aug-25</td><td>Mon</td><td>P</td><td>P</td><td>P</td><td>P</td></tr
