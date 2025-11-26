3d91453fccf6723e8c09be6bd85109e011ac14d7
VARDHAN_VALLURI <krishnavardhan124@gmail.com>
2025-11-26 13:06:43 +0530

    Remove title from student dashboard
    Removed title from the dashboard page.

--- dashboard.php
+++ dashboard.php
@@ -10,7 +10,7 @@ if (!isset($_SESSION['student'])) {
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width,initial-scale=1">
-<title>STUDENT DASHBOARD</title>
+<title> </title>
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css" rel="stylesheet">
