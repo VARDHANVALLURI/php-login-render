f96fd31f5dcc9c1e134d4ba7e335252c3e203a84
VARDHAN_VALLURI <krishnavardhan124@gmail.com>
2026-01-03 16:35:41 +0530

    Update dashboard.php

--- student/dashboard.php
+++ student/dashboard.php
@@ -5,12 +5,12 @@ include __DIR__ . "/includes/auth.php";
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Dashboard</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Dashboard</title>
 
-<link rel="stylesheet" href="../assets/css/common.css">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+  <link rel="stylesheet" href="../assets/css/common.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
 </head>
 
 <body>
